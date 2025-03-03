e8408d13ee12ebd052b656cdcdaa00f926b54e9a
Bintaryam <bintaryam.alz@gmail.com>
2025-03-03 09:46:08 +0400

    Completion of 17_tests 1-3

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -13,11 +13,14 @@ fn main() {
 mod tests {
     // TODO: Import `is_even`. You can use a wildcard to import everything in
     // the outer module.
+    use super::*;
 
     #[test]
     fn you_can_assert() {
         // TODO: Test the function `is_even` with some values.
-        assert!();
-        assert!();
+        let result = is_even(5);
+        let result2 = is_even(4);
+        assert!(!result);
+        assert!(result2);
     }
 }
